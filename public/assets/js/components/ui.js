4fc0c86a31a996e33e6f3991f3e2cc0a1193ebe3
Colin McArthur <colinmcarthur@Mac.lan>
2025-06-22 14:25:09 -0700

    Update package.json with new scripts and dependencies

--- public/assets/js/components/ui.js
+++ public/assets/js/components/ui.js
@@ -36,7 +36,6 @@
       Documented: "bg-blue-500",
       "Dev Tools": "bg-gray-500",
       "Component Design": "bg-purple-500",
-      "Coming Soon": "bg-yellow-500 text-white",
     };
 
     // Create the badge element
